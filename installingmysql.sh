d5a49377733b6645aaf1f33bc09218f2985a309f
amu94777 <amu94777@gmail.com>
2023-12-14 13:34:01 +0530

    installing mysql

---
+++ installingmysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo " now i am installing my sql "
+echo "first i need to check wther i am a root user or not"
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+    echo " ERROR: ur not a root :run with root user "
+    exit 1
+else
+    echo " u r a root user "
+fi        
+echo " I AM INSTALLING MYSQL "
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+    echo " ERROR::mysql is not installing "
+    exit 1
+else
+    echo " mysql is installed successfully "
+fi    
