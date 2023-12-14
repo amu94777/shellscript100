28ab6d5b236c747c7b9fa2e21bf19510aa6caf6d
amu94777 <amu94777@gmail.com>
2023-12-14 17:01:34 +0530

    enabaling colours

---
+++ colours.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo -e  "we are learning $R DEVOPS $N and we are all become $G DEVOPS ENGINEERS $N and we placed in a big $Y MNC $N company"
