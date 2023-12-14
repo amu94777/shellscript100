ce6ea14d1095523e66d2569760e30361010bc467
amu94777 <amu94777@gmail.com>
2023-12-14 15:46:58 +0530

    checking total number and wt all argments passed

---
+++ alltolarg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+num1="7"
+num2="6"
+sum=$(($num1+$num2))
+echo "total is :::: $sum"
+echo "print total number of argments passed:::$#"
+echo "print  what are all arguments passed::::$@"
