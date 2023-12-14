09f7c83c9fce1c5811e7ab410f03d49a9fc4a8cc
amu94777 <amu94777@gmail.com>
2023-12-14 15:19:19 +0530

    passing argments for function

---
+++ argforfun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ID=$(id -u)
+VALIDATE(){
+if [ $1 -ne 0 ] 
+then
+    echo" ERROR::::$2 package not installed "
+    exit 1
+else
+    echo" $2 package is  installed " 
+fi       
+}
+if [ $ID -ne 0 ]
+then 
+    echo"ur not a root user run with root user"
+    exit 1
+ else
+    echo"ur a root user"    
+fi    
+yum install mysql -y
+VALIDATE $? "mysql"
+yum install git -y
+VALIDATE $? "git"
