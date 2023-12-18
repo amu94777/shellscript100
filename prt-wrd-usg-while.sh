32469f86c8025036cbdad783d45bbd934c7f2741
amu94777 <amu94777@gmail.com>
2023-12-18 19:30:42 +0530

    using while loops

--- prt-wrd-usg-while.sh
+++ prt-wrd-usg-while.sh
@@ -7,4 +7,11 @@ then
 else
     echo "The $FILE is exist"
 fi
-        
+
+while IFS=":" read -r username password user_id group_id
+do
+   echo "username: $username"
+   echo "password: $password"
+   echo "user_id: $user_id"
+   echo "group_id: $group_id"
+done < $FILE    
