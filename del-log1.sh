0c75f1daaeb05ef09de25caf4be4fe8e5dffaf70
amu94777 <amu94777@gmail.com>
2023-12-18 19:45:53 +0530

    log file updation

--- del-log1.sh
+++ del-log1.sh
@@ -6,7 +6,7 @@ then
 else
     echo "$SOU_PATH is exist"
 fi        
-DELETE_FILES=$(find $SOU_PATH -type f -mtime +10 -name "*.java")
+DELETE_FILES=$(find $SOU_PATH -type f -mtime +2 -name "*.java")
 
 while IFS= read -r line
 do
