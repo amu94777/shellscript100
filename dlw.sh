c7b77be53ad50bb9ff3fad2ecbca26bde6a045bc
amu94777 <amu94777@gmail.com>
2023-12-18 12:48:03 +0530

    logfile deletion by using while loop disscussion

--- dlw.sh
+++ dlw.sh
@@ -5,5 +5,5 @@ if [ ! -d $SOURCE-PATH ]
 then
     echo "$SOURCE_PATH directory is not exist"
 fi
-FILES-TO-DEL=$(find $SOURCE_PATH -type f +10 -name "*.java")
+FILESTODEL=$(find $SOURCE_PATH -type f +10 -name "*.java")
     
