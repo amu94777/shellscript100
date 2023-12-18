ecedd823bc629c16580fc06d26a0d591cd6709e3
amu94777 <amu94777@gmail.com>
2023-12-18 12:45:59 +0530

    logfile deletion by using while loop disscussion

--- dlw.sh
+++ dlw.sh
@@ -1,9 +1,9 @@
 ###delete lofile using while loop
 #!/bin/bash
-SOURCE_PATH="/tmp/shell-logs"
+SOURCE_PATH="/tmp/alllogs"
 if [ ! -d $SOURCE-PATH ]
 then
     echo "$SOURCE_PATH directory is not exist"
 fi
-FILES-TO-DEL=$(find $SOURCE_PATH -type f +10 -name "*.log")
+FILES-TO-DEL=$(find $SOURCE_PATH -type f +10 -name "*.java")
     
