b22c00f914e23d333795e3ef098ac64e773fde30
amu94777 <amu94777@gmail.com>
2023-12-18 22:44:03 +0530

    disk usage

---
+++ diskusg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DISK_USG=$(df -hT | grep -Ev 'tmp|File')
+THESH_HOLD=1
+while IFS= read line
+do
+   USAGE=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+   PARTITION=$(echo $line | awk '{print $1F}')
+   if [ $USAGE -ge THESH_HOLD ]
+   then
+    message+="HIGH DISK USAGE ON $PARTITION:$USAGE"
+   fi 
+done <<< $DISK_USG
+echo "message::$message"
