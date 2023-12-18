ee7ca1058ee7e6d764ecf8ff5503310520af1648
amu94777 <amu94777@gmail.com>
2023-12-18 11:51:41 +0530

    logfile deletion disscussion

---
+++ dellog.sh
@@ -0,0 +1,8 @@
+######dDELETE LOG FILES IN A DIRECTORY######
+#!/bin/bash
+DIRNAME="ammu"
+if [ ! -d $DIRNAME ]
+then
+    echo "the directory is not exist"
+fi
+find $DIRNAME -type f -mtime +10 -name ".t" -exec rm {} \;
