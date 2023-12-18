85c8007f4f591ea991b590db702a2883e94ba285
amu94777 <amu94777@gmail.com>
2023-12-18 14:16:23 +0530

    log file disscuss

--- del-log1.sh
+++ del-log1.sh
@@ -6,3 +6,10 @@ then
 else
     echo "$SOU_PATH is exist"
 fi        
+DELETE_FILES=$(find $SOU_PATH -type f -mtime +10 -name "*.java")
+echo "$DELETE_FILES"
+while IFS= read -r line
+do
+  echo "Deleting first $line
+  rm -rf $line
+done <<< $DELETE_FILES  
