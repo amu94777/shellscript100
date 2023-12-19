dffaf0206e801e292ec5657747a1d7e92e247a5e
amu94777 <amu94777@gmail.com>
2023-12-19 17:18:07 +0530

    case script

---
+++ case.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+NAME=""
+WISHES=""
+USAGE(){
+    echo "USAGE::::$(basename $0) -n NAME -w WISHES -h USAGE"
+    echo "OPTIONS"
+    echo "-n, -n is name we should enter name"
+    echo "-w, -w is wishes msg"
+    echo "-h, -h display help and exit"
+
+}
+while getops ":n:w:h" opt; do
+case $opt in 
+ n) NAME="$OPTARG"
+ w) WISHES="$OPTARG"
+ h|*) USAGE; exit;;
+ esac
+ done
+
