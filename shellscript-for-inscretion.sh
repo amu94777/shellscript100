b73c7038458c92a3b0ee6ed362f87781ed6056f2
amu94777 <amu94777@gmail.com>
2023-12-24 14:51:47 +0530

    create instances using ansible

---
+++ shellscript-for-inscretion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+AMI= "ami-03265a0778a880afb"
+SG= "sg-089f2b87f4fcb3a7e"
+INSTANCES=("mongodb" "mysql" "redis" "web" "shipping" "payment" "rabbitmq" 
+           "cart" "catalogue" "user" "dispatch")
+for i in ${INSTANCES[0]}
+do 
+   echo "$i" 
+   if [ $i == mongodb ] || [ $i == mysql ] || [ $i == shipping ] 
+   then
+     INSTANCE_TYPE= "t3.small"        
+   else
+     INSTANCE_TYPE= "t2.micro"
+   fi
+aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-089f2b87f4fcb3a7e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+done        
